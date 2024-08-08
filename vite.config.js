7898b2877fcc3a160abf8ceb9ccb0ae8f41ca8bd
hector <e3.corefloriambra@proton.me>
2024-08-08 16:47:40 -0500

    se cambia estilos para ser responsive

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://e3core.github.io/practica-de-multiplicar"
 })
